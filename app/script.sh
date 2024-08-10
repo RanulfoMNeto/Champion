79e9e769f6933319793a0b3c217453f4bb9226cd
RanulfoMNeto <ranulfomascari@hotmail.com>
2024-08-10 06:43:40 -0300

    Modificando estrutura do Dockerfile

--- app/script.sh
+++ app/script.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Define variables
+URL="https://u.gg/lol/champions/mordekaiser/build"
+OUTPUT_FILE="index.html"
+PYTHON_PATH="/opt/venv/bin/python"
+SCRIPT="main.py"
+
 # Use curl to download the HTML page
-curl -o "index.html" https://u.gg/lol/champions/mordekaiser/build
+curl -o "$OUTPUT_FILE" "$URL"
 
 # Run the parser script using the virtual environment's Python
-/opt/venv/bin/python parser.py
+"$PYTHON_PATH" "$SCRIPT"
